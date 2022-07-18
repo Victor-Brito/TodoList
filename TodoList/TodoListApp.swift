b62ec912abe53345e71b2e0fb58d6593808539a4
Victor de Brito Marinho Correa <victordevelopersoftware@gmail.com>
2022-07-18 14:19:10 -0300

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,20 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Victor Brito on 18/07/22.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}

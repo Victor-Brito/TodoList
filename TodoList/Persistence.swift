45f6c4ea1b2f42ec29af7d5a9ab582c585dfb679
Igor Silva <igorsilvadeveloper@gmail.com>
2022-07-18 15:27:31 -0300

    feat: create core data methods and unit tests

--- TodoList/Persistence.swift
+++ TodoList/Persistence.swift
@@ -7,29 +7,17 @@
 
 import CoreData
 
-struct PersistenceController {
+class PersistenceController {
+    
     static let shared = PersistenceController()
-
-    static var preview: PersistenceController = {
+    static var temp: PersistenceController = {
         let result = PersistenceController(inMemory: true)
         let viewContext = result.container.viewContext
-        for _ in 0..<10 {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-        }
-        do {
-            try viewContext.save()
-        } catch {
-            // Replace this implementation with code to handle the error appropriately.
-            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-            let nsError = error as NSError
-            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-        }
         return result
     }()
-
+    
     let container: NSPersistentContainer
-
+    
     init(inMemory: Bool = false) {
         container = NSPersistentContainer(name: "TodoList")
         if inMemory {
@@ -37,20 +25,47 @@ struct PersistenceController {
         }
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-
-                /*
-                 Typical reasons for an error here include:
-                 * The parent directory does not exist, cannot be created, or disallows writing.
-                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
-                 * The device is out of space.
-                 * The store could not be migrated to the current model version.
-                 Check the error message to determine what the actual problem was.
-                 */
                 fatalError("Unresolved error \(error), \(error.userInfo)")
             }
         })
         container.viewContext.automaticallyMergesChangesFromParent = true
     }
+    
+    func createActivity(name: String) -> Activity {
+        let activity = Activity(context: container.viewContext)
+        activity.timestamp = Date()
+        activity.name = name
+        save()
+        return activity
+    }
+    
+    func updateActivity(activity: Activity, name: String, timeStamp: Date? = nil) -> Activity {
+        if let timeStamp = timeStamp {
+            activity.timestamp = timeStamp
+        }
+        activity.name = name
+        save()
+        return activity
+    }
+    
+    func save() {
+        do {
+            try container.viewContext.save()
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func fetchActivities() -> [Activity] {
+        do {
+            return try container.viewContext.fetch(Activity.fetchRequest())
+        } catch {
+            return []
+        }
+    }
+    
+    func delete(object: NSManagedObject) {
+        container.viewContext.delete(object)
+    }
+    
 }
--- TodoListTests/TodoListTests.swift
+++ TodoListTests/TodoListTests.swift
@@ -9,7 +9,7 @@ import XCTest
 @testable import TodoList
 
 class TodoListTests: XCTestCase {
-
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -18,19 +18,32 @@ class TodoListTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
         }
+    }
+    
+    func testFetchActivities() throws {
+        let persistenceController = PersistenceController.temp
+        let _ = persistenceController.createActivity(name: "Activity 1")
+        let activities = persistenceController.fetchActivities()
+        XCTAssertEqual(activities.count, 1)
+    }
+    
+    func testCreateActivity() throws {
+        let persistenceController = PersistenceController.temp
+        let activity = persistenceController.createActivity(name: "New Activity")
+        XCTAssertEqual(activity.name, persistenceController.fetchActivities().first!.name)
+    }
+    
+    func testDeleteActivity() throws {
+        let persistenceController = PersistenceController.temp
+        let activity = persistenceController.createActivity(name: "Activity Delete")
+        XCTAssertEqual(persistenceController.fetchActivities().count, 1)
+        persistenceController.delete(object: activity)
+        XCTAssertEqual(persistenceController.fetchActivities().count, 0)
     }
 
 }
